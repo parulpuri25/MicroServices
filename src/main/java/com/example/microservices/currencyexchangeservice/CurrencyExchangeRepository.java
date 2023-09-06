d2ecde5f73eab2644eea10c7244e1b18bf76144f
Parul Puri <93627829+parulpuri25@users.noreply.github.com>
2023-09-05 23:35:10 -0400

    Delete src/main/java/com/example/microservices/currencyexchangeservice directory

--- src/main/java/com/example/microservices/currencyexchangeservice/CurrencyExchangeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.microservices.currencyexchangeservice;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long>{
-	CurrencyExchange findbyFromAndTo(String from, String to);
-}
