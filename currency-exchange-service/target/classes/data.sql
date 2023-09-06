eee4f4c1678ceff5018a0698ac3e4c781108f728
Parul Puri <93627829+parulpuri25@users.noreply.github.com>
2023-09-05 23:36:18 -0400

    Add files via upload

---
+++ currency-exchange-service/target/classes/data.sql
@@ -0,0 +1,11 @@
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(1000,'USD','INR',65,'');
+
+insert into CURRENCY_EXCHANGE
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(1001,'CAD','INR',62,'');
+
+insert into CURRENCY_EXCHANGE
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(1002,'AUS','INR',60,'');
